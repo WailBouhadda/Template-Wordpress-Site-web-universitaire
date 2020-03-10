2673d8125aaee512b1ffc36757da63ef6f046787
WAIL-20 <wailbouhadda1@gmail.com>
2020-03-10 17:51:11 +0100

    Add functions

--- Functions.php
+++ Functions.php
@@ -1,4 +1,9 @@
 <?php
 
-
+function word_work(){
+   
+    wp_enqueue_script('js',get_theme_file_uri('script/js.js',NULL,'3.1.1',true));
+    wp_enqueue_style('style_css',get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts','word_work');
 ?>
