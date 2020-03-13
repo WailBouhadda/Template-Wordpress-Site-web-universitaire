ecf7ff452676c320ab33c6effcde61275f2baeec
Aimene <aimennouri65@gmail.com>
2020-03-13 12:21:21 +0100

    update style.css

--- Header.php
+++ Header.php
@@ -6,7 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>EST Sidi Bennour</title>  
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?php  echo get_template_directory_uri().'/mystyle.css'?>"> 
+  <link rel="stylesheet" href="<?php  echo get_template_directory_uri().'/style.css'?>"> 
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
   <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
   <link rel="icon" href="../EST_SidiBennour.ico" />
