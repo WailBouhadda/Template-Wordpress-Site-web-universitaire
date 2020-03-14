8a9fc0e9996d336da16eccf82a29fa21ad2f4788
Aimene <aimennouri65@gmail.com>
2020-03-14 23:04:55 +0100

    create main.css and update style.css

--- Functions.php
+++ Functions.php
@@ -1,7 +1,6 @@
 <?php
 function add_style(){
-    wp_enqueue_style( 'bootstrap-css',    get_template_directory_uri(  )."/css/bootstrap.min.css" );
-    wp_enqueue_style( 'fontawesome-css',    get_template_directory_uri()."/css/font-awesome.min.css");
+    wp_enqueue_style('style_css',get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts','all_scripts_and_style');
 ?>
