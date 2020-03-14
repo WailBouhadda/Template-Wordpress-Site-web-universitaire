6244b85189b6534265173a27056b3b5589bd04f4
Aimene <aimennouri65@gmail.com>
2020-03-15 00:16:33 +0100

    modify functions.php

--- Functions.php
+++ Functions.php
@@ -1,5 +1,7 @@
 <?php
 function add_style(){
+    wp_enqueue_style( 'bootstrap-css',    get_template_directory_uri(  )."/css/bootstrap.min.css" );
+    wp_enqueue_style( 'fontawesome-css',    get_template_directory_uri()."/css/font-awesome.min.css");
     wp_enqueue_style('style_css',get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts','add_style');
