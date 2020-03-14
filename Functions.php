e112579828863b699b0e31881bbd82fe639e0325
Aimene <aimennouri65@gmail.com>
2020-03-15 00:14:15 +0100

    modify functions.php

--- Functions.php
+++ Functions.php
@@ -2,5 +2,5 @@
 function add_style(){
     wp_enqueue_style('style_css',get_stylesheet_uri());
 }
-add_action('wp_enqueue_scripts','all_scripts_and_style');
+add_action('wp_enqueue_scripts','add_style');
 ?>
