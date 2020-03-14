284c56c461bc0fb78580a1ca68d7621e6e538e55
Aimene <aimennouri65@gmail.com>
2020-03-14 22:56:27 +0100

    styling the footer

--- Footer.php
+++ Footer.php
@@ -1,7 +1,7 @@
-        <footer class="Footer">
+        <div class="Footer">
             <p>Powered with  ❤️ By <a href="https://github.com/WaIl-20">Wail</a> & <a href="https://github.com/AimeneNouri">Aimene</a></p>
             <p>&copy;<script>document.write(new Date().getFullYear());</script>, All rights are reserved for <a href="<?php bloginfo('url'); ?>" target="_blank"><?php bloginfo('name'); ?></a></p>
-        </footer>     
+        </div>       
 </body>
 <script type="text/javascript">
 (function($) { "use strict";
