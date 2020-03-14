00d08398e5cba2b9256c37b85d8d9aa0d7863b36
Aimene <aimennouri65@gmail.com>
2020-03-14 22:19:05 +0100

    adding some style in function.php

--- Functions.php
+++ Functions.php
@@ -1,5 +1,7 @@
 <?php
-function all_scripts_and_style(){
+function add_style(){
+    wp_enqueue_style( 'bootstrap-css',    get_template_directory_uri(  )."/css/bootstrap.min.css" );
+    wp_enqueue_style( 'fontawesome-css',    get_template_directory_uri()."/css/font-awesome.min.css");
 }
 add_action('wp_enqueue_scripts','all_scripts_and_style');
 ?>
