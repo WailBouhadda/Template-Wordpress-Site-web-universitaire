f9e34c7ce86ecc0630e1bafecb5fb6b3cc47886d
WAIL-20 <wailbouhadda1@gmail.com>
2020-03-18 12:03:40 +0100

    change sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,41 +1,10 @@
+<div class="col-lg-3">
 
+<?php if(is_active_sidebar('page-sidebar') ):?>
 
+    <?php dynamic_sidebar('page-sidebar');?>
 
+<?php endif;?>
 
-<div class="side_bar">
-
-<div class="Contact_form">
-    <form id="Contact_form" method="post" action="">
-        <input name="name" type="text" class="form_control" placeholder="Votre nom..." required><br>
-        <input name="email" type="text" class="form_control" placeholder="Votre email..." required><br>
-        <textarea name="message" class="form_control" placeholder="Message..." id="" cols="30" rows="5" required></textarea><br>
-
-        <input type="submit" class="form_control submit" value="Envoyer ." >
-    </form>
-</div>
-
-
-</div>
     
-<?php
-$nom = $_POST['nom'];
-$visiteur_email = $_POST['email'];
-$message = $_POST['message'];
-
-$email_from = 'ESTSB.ucd.ma@gmail.com';
-$email_subject = "Contact us";
-$email_body = "User name : $name.\n".
-                "User email : $email.\n".
-                    "User message : $message.\n";
-
-
-  $to = "wailbouhadda1@gmail.com";
-  
-  $headers ="From : $email_from \r\n";
-  $headers .= "Replay to : $visiteur_email \r\n";
-
-  mail($to,$email_subject,$email_body,$headers);
-
-
-
-?>
+</div>
