b81bac586dc3a1360940c99b3bb1e375fe0367d8
WAIL-20 <wailbouhadda1@gmail.com>
2020-06-27 21:50:16 +0100

    Update Header.php

--- Header.php
+++ Header.php
@@ -36,22 +36,22 @@
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Ecole</a>
                     <div class="dropdown-menu">
                       <a class="dropdown-item" href="Presentation">Presentation</a>
-                      <a class="dropdown-item" href="Mots de directeur">Mots de directeur</a>
-                      <a class="dropdown-item" href="Corps Enseignants">Corps Enseignants</a>
+                      <a class="dropdown-item" href="mots-de-directeur">Mots de directeur</a>
+                      <a class="dropdown-item" href="corps-enseignants">Corps Enseignants</a>
                     </div>
                   </li>
                   <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Formations</a>
                     <div class="dropdown-menu">
-                      <a class="dropdown-item" href="Formation Initial">Formation Initial</a>
-                      <a class="dropdown-item" href="Formation Continue">Formation Continue</a>
+                      <a class="dropdown-item" href="formation-initial">Formation Initial</a>
+                      <a class="dropdown-item" href="formation-continue">Formation Continue</a>
                     </div>
                   </li>
                   <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Espace Etudiant</a>
                     <div class="dropdown-menu">
-                      <a class="dropdown-item" href="Reglement Des Etudes">Reglement Des Etudes</a>
-                      <a class="dropdown-item" href="Planning Annuel">Planning Annuel</a>
+                      <a class="dropdown-item" href="reglement-des-etudes">Reglement Des Etudes</a>
+                      <a class="dropdown-item" href="planning-annuel">Planning Annuel</a>
                       <a class="dropdown-item" href="Bourse">Bourse</a>
                       <a class="dropdown-item" href="Emploi Du Temps A.U.2019-2020">Emploi Du Temps A.U.2019-2020</a>
                     </div>
